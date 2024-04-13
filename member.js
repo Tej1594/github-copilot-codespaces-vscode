ca5a096078f9d435284bc919fa1aa2abe7fd1100
Tej1594 <166800452+Tej1594@users.noreply.github.com>
2024-04-13 05:46:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillMember() {
+  return {
+    id: 1,
+    name: 'member',
+    skills: [
+      { id: 1, name: 'skill1' },
+      { id: 2, name: 'skill2' },
+      { id: 3, name: 'skill3' },
+    ],
+  };
+}
